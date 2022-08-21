cc5cc1e2b0dd02af9e76b6a8eac07f3a9be441cb
신지훈 <rec8730@gmail.com>
2022-08-21 23:26:25 +0900

    ADD : 디테일페이지 영역 지정

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -4,13 +4,11 @@ import './Detail.scss';
 
 const Detail = ({office}) => {
   const {id} = useParams();
-  console.log(id)
-  console.log(office);
   return(
     <div className='Detail'>
       <div className='images'>
         <div className='image-black' />
-        <img className='image' src='../images/office01.jpg' alt='디테일 이미지 불러오기 실패'/>
+        <img className='image' src='../images/office01.jpg' alt='이미지 불러오기 실패'/>
         <div className='title-content'>
           <h4 className='title'>{office[id]?.title}</h4>
           <p className='content'>{office[id]?.special}</p>
@@ -19,6 +17,9 @@ const Detail = ({office}) => {
           <div><a className='map' href={office[id]?.place_url} target="_blank" rel="noopener noreferrer">지도보기</a></div>
           <div><a className='homepage' href={office[id]?.page_url} target="_blank" rel="noopener noreferrer">홈페이지</a></div>
         </div>
+      </div>
+      <div className='places'>
+        
       </div>
     </div>
   )
